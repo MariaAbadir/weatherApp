9c1a3f5b4e317d3fce205ba2e737f8d08b4566e0
MariaAbadir <mariamedhat2@gmail.com>
2025-02-11 20:06:13 +0200

    dockerfile:   Dockerfile 	inventoryfile:   ansible/inventory 	playbookfile:   ansible/playbook.yml 	modified:   app.py 	jenkinsfile:   jenkinsfile

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Replace with your OpenWeatherMap API key
-API_KEY = "your_openweathermap_api_key"
+API_KEY = "59a50e82bfb37785972a20ccc98e67e3"
 
 # SQLite database path
 DATABASE = "weather.db"
